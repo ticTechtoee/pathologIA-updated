6cdcabf6b947679083fc8ca935d32911c024f303
Farhan Basheer <farhanfb6@outlook.com>
2023-03-04 01:48:05 +0500

    Image Module completed

--- ImagesApp/urls.py
+++ ImagesApp/urls.py
@@ -6,4 +6,7 @@ app_name = 'ImagesApp'
 urlpatterns = [
     path('CreateImage/', views.ViewCreateImage, name='CreateImageView'),
     path('ImagesGrid/<str:pk>', views.ViewImagesGrid, name='GridImages'),
+    path('DeleteImage/<str:pk>', views.ViewDeleteImage, name='DeleteImageView'),
+    path('EditImage/<str:pk>', views.ViewEditImage, name='EditImageView'),
+
 ]
--- ImagesApp/views.py
+++ ImagesApp/views.py
@@ -11,6 +11,11 @@ def ViewCreateImage(request):
                 group_name = deleteForm.cleaned_data.get('Image_Group')
                 print(group_name)
                 return redirect(reverse('ImagesApp:GridImages', kwargs={'pk': group_name}))
+        elif 'btnSubmitFormEdit' in request.POST:
+            if deleteForm.is_valid():
+                group_name = deleteForm.cleaned_data.get('Image_Group')
+                print(group_name)
+                return redirect(reverse('ImagesApp:GridImages', kwargs={'pk': group_name}))
         else:
             form = CreateImageForm(request.POST,request.FILES)
             if form.is_valid():
@@ -18,17 +23,25 @@ def ViewCreateImage(request):
     context = {'form': form, 'delteImageform':deleteForm}
     return render(request, 'ImagesApp/Index.html', context) 
 
-def ViewDeleteImage(request):
-    form = CreateImageForm(request.POST or None)
-    if request.method == 'POST':
-        form = CreateImageForm(request.POST,request.FILES)
-        if form.is_valid():
-            form.save()
-    context = {'form': form}
-    return render(request, 'ImagesApp/Index.html', context) 
 
 def ViewImagesGrid(request, pk):
     selected_group = ImageGroupModel.objects.get(Description=str(pk))
     selected_group_images = ImageModel.objects.filter(Image_Group=selected_group.Id_Group)
     context = {'list_of_images':selected_group_images}
     return render(request, 'ImagesApp/ImagesGrid.html', context)
+
+def ViewDeleteImage(request, pk):
+    image_to_delete = ImageModel.objects.get(Id_Image = str(pk))
+    image_to_delete.delete()
+    return redirect('ImagesApp:CreateImageView') 
+
+def ViewEditImage(request,pk):
+    image_obj = ImageModel.objects.get(Id_Image = pk)
+    form = CreateImageForm(instance=image_obj)
+    if request.method == 'POST':
+        form = CreateImageForm(request.POST, request.FILES, instance=image_obj)
+        if form.is_valid():
+            form.save()
+            return redirect('ImagesApp:CreateImageView')
+    context = {'form':form}
+    return render(request, 'ImagesApp/Index.html', context)
