78a74b7e8d3325eadec018797cfd8fbb2d06ea47
Farhan Basheer <farhanfb6@outlook.com>
2023-04-18 15:59:00 +0500

    changed DB to Sqlite

--- pathologIA/settings.py
+++ pathologIA/settings.py
@@ -81,12 +81,19 @@ WSGI_APPLICATION = 'pathologIA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': 'sql.cnf',
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': 'sql.cnf',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
