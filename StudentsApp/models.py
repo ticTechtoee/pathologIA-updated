f7c4e6b950d50e8ae86834b3f2843351a5f25148
Farhan Basheer <farhanfb6@outlook.com>
2023-05-21 20:38:17 +0500

    Temp Alteration

--- StudentsApp/models.py
+++ StudentsApp/models.py
@@ -8,6 +8,6 @@ class StudentPerformance(models.Model):
     StudentPerformance_ID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     Student_Information = models.ForeignKey('AccountsApp.CustomUserModel', on_delete=models.DO_NOTHING)
     Question_Information = models.ForeignKey('QuestionsApp.QuestionsModel', on_delete=models.CASCADE)
-    Question_Group_Information = models.ForeignKey('QuestionsApp.QuestionGroupModel', on_delete=models.DO_NOTHING, default=QuestionGroupModel.objects.first().pk)
+    #Question_Group_Information = models.ForeignKey('QuestionsApp.QuestionGroupModel', on_delete=models.DO_NOTHING, default=QuestionGroupModel.objects.first().pk)
     Score_Per_Question = models.DecimalField(max_digits=10, decimal_places=2)
     Completed_At = models.DateTimeField(auto_now_add=True)
